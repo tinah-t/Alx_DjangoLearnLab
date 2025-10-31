5e18babdb0f1af013898e6157ad6bb0be676e350
Hana <gerenett@gmail.com>
2025-10-31 17:22:07 +0300

    changed list_field to list_filter

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
 admin.site.register(Book , BookAdmin)
