1b25ddb91b6aa5472c3f68add6bd3f32d70d8643
Hana <gerenett@gmail.com>
2025-11-16 19:58:00 +0300

    no cluee

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,13 +5,13 @@ from django.contrib.auth.decorators import permission_required
 from .models import Article
 
 # Anyone with 'can_view' permission
-@permission_required('relationship_app.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def list_articles(request):
     articles = Article.objects.all()
-    return render(request, "relationship_app/list_articles.html", {"articles": articles})
+    return render(request, "bookshelf/list_articles.html", {"articles": articles})
 
 # Only users with 'can_create'
-@permission_required('relationship_app.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def create_article(request):
     if request.method == "POST":
         title = request.POST.get("title")
@@ -21,23 +21,30 @@ def create_article(request):
             content=content,
             created_by=request.user
         )
-        return render(request, "relationship_app/article_created.html", {"article": article})
-    return render(request, "relationship_app/create_article.html")
+        return render(request, "bookshelf/article_created.html", {"article": article})
+    return render(request, "bookshelf/create_article.html")
 
 # Only users with 'can_edit'
-@permission_required('relationship_app.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == "POST":
         article.title = request.POST["title"]
         article.content = request.POST["content"]
         article.save()
-        return render(request, "relationship_app/article_updated.html", {"article": article})
-    return render(request, "relationship_app/edit_article.html", {"article": article})
+        return render(request, "bookshelf/article_updated.html", {"article": article})
+    return render(request, "bookshelf/edit_article.html", {"article": article})
 
 # Only users with 'can_delete'
-@permission_required('relationship_app.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     article.delete()
-    return render(request, "relationship_app/article_deleted.html")
+    return render(request, "bookshelf/article_deleted.html")
+def book_list(request):
+    """
+    Display a list of books (or articles) in the system.
+    """
+    books = Article.objects.all()   # or Book.objects.all()
+    context = {"books": books}
+    return render(request, "bookshelf/book_list.html", context)
