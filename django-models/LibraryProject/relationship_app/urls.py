cbc12e46d374217e640f81b0b4eaae321007d4d5
Hana <gerenett@gmail.com>
2025-11-16 18:01:02 +0300

    add_book and edit_book renamed in url

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', views.list_books,name='books'),
     path('about/<int:pk>/', views.LibraryDetailView.as_view(),name='about'),
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/edit/<int:book_id>/", views.edit_book, name="edit_book"),
-    path("books/delete/<int:book_id>/", views.delete_book, name="delete_book"),
+    path("books/add_book/", views.add_book, name="add_book"),
+    path("books/edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("books/delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html')), 
     path('register/', views.register, name="register"),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'))
