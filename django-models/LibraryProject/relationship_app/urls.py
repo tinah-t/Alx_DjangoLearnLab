146493c03d19870e6cff98df9d0a1b37e8629f4e
Hana <gerenett@gmail.com>
2025-11-16 17:59:04 +0300

    Setting permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
-    path('hello/', views.list_books,name='hello'),
+    path('books/', views.list_books,name='books'),
     path('about/<int:pk>/', views.LibraryDetailView.as_view(),name='about'),
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/edit/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", views.delete_book, name="delete_book"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html')), 
     path('register/', views.register, name="register"),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'))
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from .models import Library , Book
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Library , Book, Author
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -18,8 +20,31 @@ def register(request):
     form = UserCreationForm()
     return render(request, 'relationship_app/register.html',{'form':form})
 
+permission_required("relationship_app.can_add_book")
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        author_obj = Author.objects.create(name=author)
+        Book.objects.create(title=title,author=author_obj)
+        return redirect("books")
+    return render(request, "relationship_app/add_book.html")
 
-# def SignUpView(CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
-#     template_name = 'relationship_app/register.html'
+@permission_required("relationship_app.can_change_book")
+def edit_book(request, book_id):
+    book = Book.objects.get(book_id)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.save()
+        return redirect("books")
+
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+@permission_required("relationship_app.can_delete_book")
+def delete_book(request, book_id):
+    book = Book.objects.get(book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("books")
+
+    return render(request, "relationship_app/delete_book.html", {"book": book})
