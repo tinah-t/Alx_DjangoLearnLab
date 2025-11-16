bcdaaf9b6ba2d28c969a44babb38900beb143ee4
Hana <gerenett@gmail.com>
2025-11-16 11:04:35 +0300

    made a function based register fun

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView,SignUpView
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('hello/', list_books,name='hello'),
     path('about/<int:pk>/', LibraryDetailView.as_view(),name='about'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html')), 
-    path('register/', SignUpView.as_view(), name="register"),
+    path('register/', register, name="register"),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'))
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,7 +16,11 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+def register(request):
+    return render(request, 'relationship_app/register.html')
+
+
+# def SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'relationship_app/register.html'
