b00e07591fe9b064169c7577870e0e18dbc2d9c8
Hana <gerenett@gmail.com>
2025-11-16 10:57:58 +0300

    login import in views module

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 # Create your views here.
 def list_books(request):
