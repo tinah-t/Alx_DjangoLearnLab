121c2f5e441d54a63886744aef6d35c4cf7aff8f
Hana <gerenett@gmail.com>
2025-11-27 14:04:19 +0300

    import Response from rest_framwork

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.response import Response
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
