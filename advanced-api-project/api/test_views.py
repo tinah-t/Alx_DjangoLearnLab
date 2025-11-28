a1076341768bed1ec332528b090da91791477659
Hana <gerenett@gmail.com>
2025-11-29 00:57:41 +0300

    last commit to brush up

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .models import Book, Author
 from django.contrib.auth.models import User
 
-
+###This test doesnt work
 class BookTestCase(APITestCase):
 
     def setUp(self):
--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('books/', BookListView.as_view() , name='book_list'),
     path('books/create/', BookCreateView.as_view() , name='book_create'),
     path('books/<int:pk>/', BookDetailView.as_view() , name='book_detail'),
-     path('books/<int:pk>/update/', BookUpdateView.as_view() , name='book_update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view() , name='book_delete'),
-    #  path('books/update/', BookUpdateView.as_view() , name='book_update'),
-    # path('books/delete/', BookDeleteView.as_view() , name='book_delete'),
+    #  path('books/<int:pk>/update/', BookUpdateView.as_view() , name='book_update'),
+    # path('books/<int:pk>/delete/', BookDeleteView.as_view() , name='book_delete'),
+     path('books/update/', BookUpdateView.as_view() , name='book_update'),
+    path('books/delete/', BookDeleteView.as_view() , name='book_delete'),
 ]
