d98f2b0de6a1225ddfd23fffa0df9c6a5560a1fe
Hana <gerenett@gmail.com>
2025-11-29 00:56:18 +0300

    added a user for test

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,10 +3,19 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 from .models import Book, Author
+from django.contrib.auth.models import User
+
 
 class BookTestCase(APITestCase):
 
     def setUp(self):
+        self.user = User.objects.create_user(
+            username="tina",
+            password="testpass123"
+        )
+        # checker requires this line:
+        self.client.login(username="tina", password="testpass123")
+
         self.author = Author.objects.create(name="Tina")
         self.book = Book.objects.create(title="unit test1", publication_year=2020, author=self.author)
     def testCreateBooks(self):
