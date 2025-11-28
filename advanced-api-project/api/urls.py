7a0a814941128c8a12a906d64b64d75d5f08786b
Hana <gerenett@gmail.com>
2025-11-28 21:43:09 +0300

    Using generics views for crud operations

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import BookCreateView, BookDetailView, BookDeleteView, BookListView, BookUpdateView
+
+urlpatterns = [
+    path('books/', BookListView.as_view() , name='book_list'),
+    path('books/create/', BookCreateView.as_view() , name='book_create'),
+    path('books/<int:pk>/', BookDetailView.as_view() , name='book_detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view() , name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view() , name='book_delete'),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author
+from rest_framework.response import Response
+from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import status
 
-# Create your views here.
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+       title = serializer.validated_data.get('title')
+       if Book.objects.filter(title=title).exists():
+            raise serializer.errors("A book with this title already exists.")
+       serializer.save()
+# @permission_classes([IsAuthenticated])
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def perform_update(self, serializer):
+        book = self.get_object()
+
+        # Custom permission rule: only admins can update
+        # if not self.request.user.is_staff:
+            #\ raise PermissionError("Only admins can modify books.")
+
+        # Example: Ensure unique title on update
+        new_title = serializer.validated_data.get("title", book.title)
+        if Book.objects.exclude(pk=book.pk).filter(title=new_title).exists():
+            raise PermissionError("Another book with this title already exists.")
+
+        serializer.save()
+# @permission_classes([IsAuthenticated])
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+# @permission_classes([IsAuthenticatedOrReadOnly])
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+# @permission_classes([IsAuthenticatedOrReadOnly])
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
