2f86dc21935418b592b1f67781968ea64f88337a
Hana <gerenett@gmail.com>
2025-12-03 13:23:51 +0300

    Edited the import statment for user in models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
