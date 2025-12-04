fc849080c2f205b7725ca031f62219f8e3a8bb66
Hana <gerenett@gmail.com>
2025-12-04 23:16:39 +0300

    using class based views in django for crud

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup, profile_view, edit_profile
+from .views import signup, profile_view, edit_profile, BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html', next_page='/login'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html', next_page='/list'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html', next_page='/create'), name='logout'),
     path('register/', signup, name='register'),
     path('profile/', profile_view, name='profile'),
     path('profile/edit/', edit_profile, name='edit-profile'),
+    path('posts/new/', BlogCreateView.as_view(),name='blog_create'),
+    path('posts/', BlogListView.as_view(),name='blog_list'),
+    path('posts/<int:pk>/', BlogDetailView.as_view(),name='blog_detail'),
+    path('posts/<int:pk>/edit', BlogUpdateView.as_view(),name='blog_update'),
+    path('posts/<int:pk>/delete', BlogDeleteView.as_view(),name='blog_delete'),
 ]
 
 # {% if user.is_authenticated %}
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -27,3 +28,33 @@ def edit_profile(request):
         form = UserChangeForm(instance=request.user)
 
     return render(request, "blog/edit_profile.html", {"form": form})
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/blog_create.html'
+    fields = ['title', 'content','author']
+    success_url = '/posts/'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/blog_update.html'
+    fields = ['title', 'content','author']
+    success_url = '/posts/'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'post'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/blog_delete.html'
+    fields = ['title', 'content','author']
+    context_object_name = 'post'
+    success_url = '/posts/'
