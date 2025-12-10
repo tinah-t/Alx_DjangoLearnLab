8e3b59817a65117d19086cb4da6abc5cd7c70484
Hana <gerenett@gmail.com>
2025-12-10 21:26:22 +0300

    created the posts app and added models

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+#  Comment should reference both Post (ForeignKey) and User (author), with additional fields for content, created_at, and updated_at.
+User = get_user_model()
+
+class Post(models.Model):
+    author = models.ForeignKey( User, on_delete=models.CASCADE,related_name="authors")
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=500)
+    created_at = models.DateField(auto_now_add = True)
+    updated_at = models.DateField(auto_now = True)
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey( User, on_delete=models.CASCADE,related_name="writer")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=500)
+    created_at = models.DateField(auto_now_add = True)
+    updated_at = models.DateField(auto_now = True)
+    
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
