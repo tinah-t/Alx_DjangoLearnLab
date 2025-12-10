15fb498746aa1eb2d2a6dc18b66f1066877c4b5e
Hana <gerenett@gmail.com>
2025-12-10 18:35:48 +0300

    get_user_model added to register

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -37,7 +37,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password2")
         password = validated_data.pop("password")
-        user = User(**validated_data)
+        user = get_user_model().objects.create_user(
+                password=password,
+                **validated_data
+            )
+
         user.set_password(password)
         user.save()
         Token.objects.create(user=user)
