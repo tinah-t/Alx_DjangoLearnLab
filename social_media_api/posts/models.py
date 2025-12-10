825ddc390549e1c60b964ac2f57ab01d09e078b9
Hana <gerenett@gmail.com>
2025-12-10 22:45:10 +0300

    DateField to DateTimeField

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -7,18 +7,18 @@ User = get_user_model()
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="authors")
     title = models.CharField(max_length=200)
-    content = models.TextField(max_length=500)
-    created_at = models.DateField(auto_now_add = True)
-    updated_at = models.DateField(auto_now = True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
     author = models.ForeignKey( User, on_delete=models.CASCADE,related_name="writer")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField(max_length=500)
-    created_at = models.DateField(auto_now_add = True)
-    updated_at = models.DateField(auto_now = True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
     
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
